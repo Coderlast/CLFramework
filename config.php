051a0e19625226b7a6f47328c2708761441e4221
CoderLast <52178799+Coderlast@users.noreply.github.com>
2021-01-31 22:21:28 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
+
 return  [
-    // TOKENGA @botFather bergan tokeni kiriting
     'TOKEN'=>'',
-    // Admin bo'lovchilarni Royhatini kiriting
     'admins' => ['594966461'],
-    /*
-    * Channel agar sizga 1 dan ortiq kanalni tekshirish kerak 
-    * bo'lsa ularni username yoki idsini kiriing
-    */
     'channels'=>['@','@','@'],
     /*
     * agar siz sqlite ishlatmoqchi bo'lsangiz sqlite dep yozing
