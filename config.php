e113006f19040379d37c0b3a3904e494d9028687
CoderLast <52178799+Coderlast@users.noreply.github.com>
2023-03-01 02:14:22 +0500

    updates

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'TOKEN'=>"",
+    'ADMINNAME'=>"",
+    "DataBase"=>[
+        'host'=>'hostname',
+        'user'=>'username',
+        'password'=>'password',
+        'dbname'=>'databasename',
+    ],
+];
