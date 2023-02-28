cfb3297d75cb0dc9d5b2e79c55e5d0c6150e728d
CoderLast <52178799+Coderlast@users.noreply.github.com>
2023-03-01 02:12:05 +0500

    Create bot.php

---
+++ examples/bot.php
@@ -0,0 +1,22 @@
+<?php
+require "vendor/autoload.php";
+require "config.php";
+//
+use App\CoderLast\Framework;
+use App\CoderLast\Plugins;
+//
+$bot = new Framework($config['TOKEN']);
+$plugins = new Plugins($config['TOKEN']);
+
+$home = [["Hello word!", "Hello word!"]];
+
+$input = $bot->getInput();
+if(isset($input->message)){
+	$message = $input->message;
+	$chat_id = $message->chat->id;
+	$text = $message->text;
+}
+
+if ($text == "/start"){
+    $bot->sendMessage($chat_id, "Hello World");
+}
